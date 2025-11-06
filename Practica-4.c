b92a9519147c90635a3082b6c0cd6ee86f5b5860
Nahum-MM <martinezmarinnahum7@gmail.com>
2025-11-05 19:29:46 -0600

    Practica-4.c

--- Practica-4.c
+++ Practica-4.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Función para intercambiar dos elementos
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// Función de partición tipo QuickSort (usa un pivote aleatorio)
 int partition(int arr[], int left, int right, int pivotIndex) {
     int pivotValue = arr[pivotIndex];
     swap(&arr[pivotIndex], &arr[right]);
@@ -25,7 +23,6 @@ int partition(int arr[], int left, int right, int pivotIndex) {
     return storeIndex;
 }
 
-// Implementación recursiva de Quick Select
 int quick_select(int arr[], int left, int right, int k) {
     if (left == right)
         return arr[left];
@@ -41,19 +38,16 @@ int quick_select(int arr[], int left, int right, int k) {
         return quick_select(arr, pivotIndex + 1, right, k);
 }
 
-// Función para generar arreglo aleatorio con números únicos
 void generar_arreglo(int arr[], int n) {
     for (int i = 0; i < n; i++)
         arr[i] = i + 1;
 
-    // Mezclar arreglo (Fisher-Yates)
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         swap(&arr[i], &arr[j]);
     }
 }
 
-// Función para probar Quick Select con medición de tiempo
 void probar_quick_select(int n, int k) {
     int *arr = (int *)malloc(n * sizeof(int));
     generar_arreglo(arr, n);
@@ -78,4 +72,5 @@ int main() {
     probar_quick_select(100000, 50000);
     return 0;
     system("pause");
+
 }
